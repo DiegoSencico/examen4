1577cd600cac730b132aa8fce23a940c2f939469
DiegoSencico <48455278@sencico.edu.pe>
2023-12-14 10:10:11 -0500

    se agrego un estilo para la lista creada y una funcion llamada Lista en js

--- codigo11.js
+++ codigo11.js
@@ -6,3 +6,6 @@ function Titulo(){
 function Descripcion(){
     console.log ("Empresa dedicada a la formacion en la construccion");
 }
+function lista(){
+    console.log("Se agrego una lista de las sedes que tiene SENCICO en todo el Peru");
+    }
